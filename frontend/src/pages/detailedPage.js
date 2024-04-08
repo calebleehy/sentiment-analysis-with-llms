f1b3c8f064e64c0ca965323efeaeec702be2559d
cheryltan17 <156302430+cheryltan17@users.noreply.github.com>
2024-04-08 12:54:22 +0800

    Update detailedPage.js

--- frontend/src/pages/detailedPage.js
+++ frontend/src/pages/detailedPage.js
@@ -26,24 +26,6 @@ const DetailedPage = () => {
 
     fetchData();
 
-  }, []);
-  const gxs = data.filter(item => item.bank === 'GXS') //filtering for only GXS data
-  const [filters, setFilters] = useState({});
-  const [filteredData, setFilteredData] = useState(data);
-  const columns = Object.keys(data[0]);
-  const handleFilterChange = (e, column) => {
-    const value = e.target.value;
-    setFilters((prevFilters) => ({ ...prevFilters, [column]: value }));
-  };
-  useEffect(() => {
-    const filtered = gxs.filter((row) =>
-    Object.entries(filters).every(([column, value]) => {
-      return row[column].toLowerCase().includes(value.toLowerCase());
-    })
-    );
-    setFilteredData(filtered);
-  }, [gxs, filters]);
-
     return (
         <div>
             <Navigation/>

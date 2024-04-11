711f50c8c4810d991474562323b082e960623e0c
cheryl tan <e0774478@u.nus.edu>
2024-04-11 10:48:44 +0800

    Edited aboutpage

--- frontend/src/pages/aboutPage.js
+++ frontend/src/pages/aboutPage.js
@@ -14,7 +14,28 @@ const AboutPage = () => {
                     Introduction
                 </h2>
 
-                <p className="px-3">introduction details...</p>
+                <p className="px-3">
+                    We are a group of students taking DSA3101 doing a customer feedback sentiment analysis 
+                    project for GXS Bank. Using advanced Natural Language Processing (NLP) techniques such as 
+                    Large Language Models (LLMs), we analyzed customer reviews from Google Play Store and Apple to 
+                    help GXS understand and improve customr satisfaction.
+                </p>
+            </Container>
+            <Container className="Container">
+                <h2>
+                    Key Features
+                </h2>
+                <ul>
+                  <li>
+                  <strong>Customizable Filters:</strong> Filter data by sentiment, service, and issue to focus on specific insights.
+                  </li>
+                  <li>
+                  <strong>Competitor Analysis:</strong> Compare sentiment trends with competitors to benchmark performance.
+                  </li>
+                  <li>
+                  <strong>Actionable Insights:</strong> Receive recommendations for improving customer experience based on sentiment analysis.
+                  </li>
+                  </ul>
             </Container>
 
             <Container className="Container ">

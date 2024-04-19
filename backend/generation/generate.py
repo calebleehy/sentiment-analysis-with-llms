f8ade58005949b677fc992b85d3506013bf589f8
ngkeenyung <ngkeyu@gmail.com>
2024-04-19 12:47:01 +0800

    minor updates to pipfile and readme before making cleanup branch

--- backend/generation/generate.py
+++ backend/generation/generate.py
@@ -1,31 +1,52 @@
 from llama_cpp import Llama
-import csv, json, warnings
+import csv, json, os
 import pandas as pd
+here = os.path.dirname(os.path.abspath(__file__)) # all future filepaths are relative to here
+datapath=os.path.normpath(os.path.join(here, '..\\data')) # ..\data directory
+modelpath=os.path.normpath(os.path.join(here, '..\\model\mistral-7b-instruct-v0.2.Q5_K_M.gguf')) # model file
+def load_model(ctx=2048, chatformat="chatml", **kwargs):
+    """
+    returns a loaded model object
+    args: 
+    - ctx = context window, default 2048
+    - chat_format = chatml by default to facilitate JSON generation
+    - additional arguments as specified in llama_cpp_python api can be passed in
+    """
+    if kwargs:
+        llm = Llama(
+            model_path=modelpath,
+            n_gpu_layers=-1, # Uncomment to use GPU acceleration
+            # seed=1337, # Uncomment to set a specific seed
+            n_ctx=ctx, # Uncomment to increase the context window
+            chat_format=chatformat,
+            **kwargs
+        )
+    else:
+        llm = Llama(
+            model_path=modelpath,
+            n_gpu_layers=-1, # Uncomment to use GPU acceleration
+            # seed=1337, # Uncomment to set a specific seed
+            n_ctx=ctx, # Uncomment to increase the context window
+            chat_format=chatformat
+        )
+    return llm
 
-llm = Llama(
-    model_path="./model/mistral-7b-instruct-v0.2.Q5_K_M.gguf",
-    n_gpu_layers=-1, # Uncomment to use GPU acceleration
-    # seed=1337, # Uncomment to set a specific seed
-    n_ctx=2048, # Uncomment to increase the context window
-    chat_format="chatml"
-)
-
-def generate_each_review(reviews_df, sysprompt, json_schema):
+def example_generation_sequential(query_list, sysprompt, json_schema, model, temp = 0.3):
+    """
+    an example of how inference can be done on a list of user query strings, other functions have a broad structure similar to this
+    """
     history = [{"role": "system", "content": sysprompt},]
     answers = []
-    for i in range(len(reviews_df)):
-        r = reviews_df.loc[i,'review']
-        history.append({"role": "user", "content": r})
-        completion = llm.create_chat_completion(
+    for q in query_list:
+        history.append({"role": "user", "content": q})
+        completion = model.create_chat_completion(
             messages=history,
-            temperature=0.7,
-            # MODIFY THE BELOW FORMAT TO
+            temperature=temp,
             response_format=json_schema
         )
         answer = json.loads(completion['choices'][0]['message']['content']) # gets json in string format, then converts to json
         # for k,v in answer.items():
         #answer["review"] = r
-        answer["id"] = reviews_df.loc[i,'id']
         answers.append(answer)
         history.pop()
     for a in answers:
@@ -48,3 +69,27 @@ def generate_each_review(reviews_df, sysprompt, json_schema):
 
 """
 
+def main():
+    test_sysprompt = """
+    You are a helpful assistant that outputs in JSON. You will be given a list of statements. Determine whether they are True, or False, and then provide your reasoning. 
+    """
+    test_queries = ["The sky is blue.", "Calculators can add numbers together", "Water is not wet", "My favourite colour is blue"]
+    test_schema = {
+        "type": "json_object",
+        "schema": {
+            "type": "object",
+            "properties": {
+                "truth": {"type": "boolean"},
+                "reasoning": {"type": "string"}
+            },
+            "required": ["truth", "reasoning"],
+        },
+    }
+    model = load_model()
+    test_responses = example_generation_sequential(test_queries, test_sysprompt, test_schema, model)
+    test_responses = pd.DataFrame.from_records(test_responses)
+    print(test_responses)
+
+if __name__=="__main__": 
+    main() 
+    
--- backend/readme.md
+++ backend/readme.md
@@ -1,15 +1,15 @@
 0. ensure pipenv is installed: https://pipenv.pypa.io/en/stable/index.html
 > pip install --user pipenv
+
 1. install requirements
-> pipenv install
+> cd .\backend && pipenv install
+
 2. download model
-5. download model
-> cd ..
-> mkdir model
-> huggingface-cli download TheBloke/Mistral-7B-Instruct-v0.2-GGUF mistral-7b-instruct-v0.2.Q5_K_M.gguf --local-dir .\model --local-dir-use-symlinks False
-6. cd to parent directory, start jupyterlab
-> cd ..
-> python -m jupyterlab
-7. start local inference server
-> python -m llama_cpp.server --model .\model\mistral-7b-instruct-v0.2.Q5_K_M.gguf
+> pipenv run huggingface-cli download TheBloke/Mistral-7B-Instruct-v0.2-GGUF mistral-7b-instruct-v0.2.Q5_K_M.gguf --local-dir .\model --local-dir-use-symlinks False
+
+OPTIONAL if you want a more sandbox environment to play around: 
+- start jupyterlab
+> pipenv run python -m jupyterlab
+- start local inference server
+> pipenv run python -m llama_cpp.server --model .\model\mistral-7b-instruct-v0.2.Q5_K_M.gguf
 

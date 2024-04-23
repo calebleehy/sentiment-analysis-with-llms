2394e45eae063549492f95b73c55222ad912f7f6
Jackson_wang <76025287+jianingwang124@users.noreply.github.com>
2024-04-23 22:51:53 +0800

    Delete backend/server directory

--- backend/server/flask_app/routes.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask import current_app as app, jsonify, request, make_response
-
-
-@app.route("/test")
-def testing():
-    data = {'sentence': 'this is a testing message'}
-    return jsonify(data), 200
